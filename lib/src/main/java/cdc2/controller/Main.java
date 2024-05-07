d42a52a7dc060b212b121bfc1286659486fd6cc6
dit <dit@example.com>
2024-05-07 12:23:18 +0200

    CDC2 – control 3

--- lib/src/main/java/cdc2/controller/Main.java
+++ lib/src/main/java/cdc2/controller/Main.java
@@ -2,8 +2,8 @@ package cdc2.controller;
 
 import cdc2.entities.Coche;
 import cdc2.model.JPACocheDao;
-/* Nombre:
- * UVUS:  */
+/* Nombre: José Julio Mimbrero Tena
+ * UVUS: JOSMIMTEN */
 public class Main {
 
 	public static void main(String[] args) {
